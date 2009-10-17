a634acea8ad31d991ec3b4bff5f686bbd829f003
Pedro Melo <melo@simplicidade.org>
2009-10-17 12:47:31 +0100

    Added documentation
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Browser/Open.pm
+++ lib/Browser/Open.pm
@@ -56,4 +56,76 @@ sub _search_in_path {
   return;
 }
 
+
 1;
+__END__
+
+=head1 NAME
+
+Browser::Open - open a browser in a given URL
+
+=head1 SYNOPSIS
+
+    use Browser::Open qw( open_browser );
+
+    my $ok = open_browser($url);
+    # ! defined($ok): no recognized command found
+    # $ok == 0: command found and executed
+    # $ok != 0: command found, error while executing
+
+=head1 DESCRIPTION
+
+The functions optionaly exported by this module allows you to open URLs
+in the user browser.
+
+A set of known commands is tested for presence, and the first one found
+is executed.
+
+The L<"open_browser"> uses the L<perlfunc/"system"> function to execute
+the command. If you want more control, you can get the command with the
+L<"open_browser_cmd"> function and then use whatever method you want to
+execute it.
+
+
+=head1 API
+
+All functions are B<not> exported by default. You must ask for them
+explicitly.
+
+
+=head2 open_browser
+
+    my $ok = open_browser($url);
+
+Find an appropriate command and executes it with your C<$url>.
+
+If no command was found, returns C<undef>.
+
+If a command is found, returns the exit code of the command.
+
+If no C<$url> is given, an exception will be thrown:
+C<< Missing required parameter $url >>.
+
+
+=head2 open_browser_cmd
+
+    my $cmd = open_browser_cmd();
+
+Returns the best command found to open a URL on your system.
+
+If no command was found, returns C<undef>.
+
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Pedro Melo.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
